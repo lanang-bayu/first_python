3c9e7cdddc4d42577bd01e920543eb5285d13c04
lanang-bayu <lanangbayu@gmail.com>
2022-11-02 10:41:27 +0700

    my first python codes

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ Ini adalah project dummy pertama saya dengan Python
 """
 print("Hello World")
 print("My name is Lanang")
+
+# contoh percabangan standart untuk susu dan telur
+jumlah_telur = 150
+jumlah_susu = 20
+
+print(f"Jumlah telur = {jumlah_telur} butir")
+print(f"Jumlah Susu = {jumlah_susu} botol")
+
+if jumlah_telur > 0:
+    print("Anak membeli telur")
+else:
+    print("Anak tidak membeli telur")
